4e85a14e040858495cad83a6a5214f42e196c277
Jordan Mateen <jordan.mateen@coxautoinc.com>
2022-05-09 11:12:40 -0400

    adding modal so that we can have visual feedback when characters are loaded.

--- script.js
+++ script.js
@@ -1,26 +1,64 @@
+let films = document.getElementById('sw-films');
+let modal = document.getElementById('characterModal');
+let modalContentTitle = document.getElementById('title');
+let characterList = document.getElementById('modal-content-character-list');
+let closeModal = document.getElementById('close-modal');
+
 window.addEventListener('DOMContentLoaded', async ()=>{
     let filmData; 
-    let films = document.getElementById('sw-films')
-    console.log("Star Wars Films Site")
+    films.innerHTML = showLoadingPrompt('films');
 
     await fetch('https://swapi.dev/api/films').then((resp)=>{
-        return resp.json()
+        return resp.json();
     }).then(data => {
         filmData = data
-    })
+    });
 
-    films.innerHTML = renderFilms(filmData.results)
-    
+    films.innerHTML = renderFilms(filmData.results);
 });
 
 const renderFilms = (filmsArray)=>{
     return filmsArray.map((film)=>{
-        const characters = film.characters;
-        return `<div onclick="renderCharacters('${characters}');" class="film-title">${film.title}</div>`
-    }).join('')
+        const characterUrls = film.characters;
+        return `<div onclick="renderCharacters('${characterUrls}','${film.title}');" class="film-title">${film.title}</div>`
+    }).join('');
 }
 
-function renderCharacters(characters){
-    console.log(characters.split(','));
+const getCharacters = async (characters) => {
+    const charactersUrlArray = characters.split(',');
+    const characterPromiseArray = charactersUrlArray.map(characterUrl => {
+        return fetch(characterUrl).then(resp => resp.json());
+    });
+    const charactersArray = await Promise.all(characterPromiseArray).then(resp => {
+        return resp;
+    });
+    return charactersArray;
+
 }
 
+const renderCharacters = async (charactersUrl, movieTitle) =>{
+    modal.style.display = "block";
+    modalContentTitle.innerHTML +=` ${movieTitle}`;
+    characterList.innerHTML = showLoadingPrompt('Characters');
+    const characterListArray = await getCharacters(charactersUrl);
+    const characters = characterListArray.map(character => {
+        return `<li>${character.name}</li>`;
+    }).join('');
+
+    characterList.innerHTML = characters;
+}
+
+const showLoadingPrompt = (itemsToLoad) => {
+    return `<h4>Loading ${itemsToLoad}...</h4>`;
+}
+
+closeModal.addEventListener('click', () => {
+    modal.style.display = "none";
+});
+
+// if the user clicks outside the modal close it. 
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+}
